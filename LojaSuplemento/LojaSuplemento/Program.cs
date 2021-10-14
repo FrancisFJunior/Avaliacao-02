c0f4b59488fdcfae8d80a53c23ea220188510dbb
Richardson <richardm@ucl.br>
2021-10-14 19:57:22 -0300

    Melhorando o Sistema De Recomendação

--- LojaSuplemento/LojaSuplemento/Program.cs
+++ LojaSuplemento/LojaSuplemento/Program.cs
@@ -17,7 +17,7 @@ namespace LojaSuplemento
             */
 
 
-
+            Produto listaProdutos = new Produto();
             Cliente cliente1 = new PessoaFisica(1234,"teste1", 1);
             Cliente cliente2 = new PessoaFisica(1234,"teste2", 2);
             BancoDadosClientes bancoDadosClientes = new BancoDadosClientes();
@@ -38,6 +38,16 @@ namespace LojaSuplemento
             Produto produtoteste8 = new Produto(8, "teste8",  "tetsetse", 1, 100);
             Produto produtoteste9 = new Produto(9, "teste9",  "tetsetse", 1, 100);
             Produto produtoteste10 = new Produto(10, "teste10",  "tetsetse", 1, 100);
+            listaProdutos.Produtos.Add(produtoteste);
+            listaProdutos.Produtos.Add(produtoteste2);
+            listaProdutos.Produtos.Add(produtoteste3);
+            listaProdutos.Produtos.Add(produtoteste4);
+            listaProdutos.Produtos.Add(produtoteste5);
+            listaProdutos.Produtos.Add(produtoteste6);
+            listaProdutos.Produtos.Add(produtoteste7);
+            listaProdutos.Produtos.Add(produtoteste8);
+            listaProdutos.Produtos.Add(produtoteste9);
+            listaProdutos.Produtos.Add(produtoteste10);
 
             teste.CarrinhoCliente.Add(produtoteste);
             teste.CarrinhoCliente.Add(produtoteste2);
@@ -58,6 +68,8 @@ namespace LojaSuplemento
 
             cliente1.AtualizaHistoico(teste.CarrinhoCliente);
             cliente2.AtualizaHistoico(teste1.CarrinhoCliente);
+
+            HelperManipulaProduto.RecebeEstoque(listaProdutos.Produtos);
 
             var testando = recomendarPorUsuario.ComparaClientes(cliente1, bancoDadosClientes);
             var testando2 = recomendarPorUsuario.getHistoricoClienteMaiorAfinidade(testando, bancoDadosClientes);
